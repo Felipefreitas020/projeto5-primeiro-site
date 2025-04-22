89fbb391d5b44e8c0b0f089f3f9192dbe558ecf1
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2025-04-22 15:44:16 -0300

    Add files via upload

---
+++ projeto5-primeiro-site/projeto5/src/App.jsx
@@ -0,0 +1,18 @@
+
+export default function App() {
+  return (
+   
+   <div>
+    <h1>React</h1>
+   </div>
+
+   <div>A biblioteca para interfaces de usuario web e nativas</div>
+   <div>
+    <button>Aprenda React</button>
+   <button>Referência da API</button>
+   </div>
+
+    
+  )
+}
+
